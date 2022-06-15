4331ce659197ac10f5a101e8bf70fa45ed03a40e
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-15 21:30:22 +0900

    added models in view

---
+++ my_site/office/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+	path('', views.list_patient, name ='list_patients')
+]
--- my_site/office/views.py
+++ my_site/office/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def list_patient(request):
+	all_patients = models.Patient.objects.all()
+	context_list = {'patients':all_patients}
+	return render(request,'office/list.html', context=context_list)
+
